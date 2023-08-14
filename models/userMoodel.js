7259a33e8f728b06cb570c862abe6d535911dc9d
aashishrana <aashishrana71135@gmail.com>
2023-08-13 22:58:34 -0700

    Working with model, schema

---
+++ models/userMoodel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        require : [true , "Name is required"];
+        trim : true,
+        maxLength : [20, "Name must less than 20 Character"]
+    },
+
+    email : {
+        type : String,
+        required : [true, "Email is required"],
+        unique : true
+    }
+})
+
+module.exports = mongoose.model("User", userSchema);
